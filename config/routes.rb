ab390e902c94d7b2ee1a979a8d248604f389232e
BDFarrey <bethanyfarrey@yahoo.com>
2017-02-11 12:36:50 -0500

    controller modification

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   get 'tencards',to: 'game#tencards'
 
+  get 'twentycards',to: 'game#twentycards'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
