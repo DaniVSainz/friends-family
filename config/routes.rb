fd81a2414cdb8055cb2c9e56051156e26a984abe
BDFarrey <bethanyfarrey@yahoo.com>
2017-02-12 11:20:01 -0500

    update pictures button and page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   get 'completed',to: 'welcome#completed'
 
+  get 'update',to: 'pictures#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
